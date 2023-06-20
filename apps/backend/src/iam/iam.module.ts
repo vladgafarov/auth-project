fe01f1ca77d00f83e4745e791a878df335fd0626
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-06-20 20:29:17 +0500

    add auth guard

--- apps/backend/src/iam/iam.module.ts
+++ apps/backend/src/iam/iam.module.ts
@@ -9,6 +9,7 @@ import jwtConfig from './config/jwt.config'
 import { ConfigModule } from '@nestjs/config'
 import { APP_GUARD } from '@nestjs/core'
 import { AccessTokenGuard } from './authentication/guards/access-token.guard'
+import { AuthenticationGuard } from './authentication/guards/authentication.guard'
 
 @Module({
 	imports: [
@@ -22,8 +23,9 @@ import { AccessTokenGuard } from './authentication/guards/access-token.guard'
 		},
 		{
 			provide: APP_GUARD,
-			useClass: AccessTokenGuard,
+			useClass: AuthenticationGuard,
 		},
+		AccessTokenGuard,
 		AuthenticationService,
 		PrismaService,
 	],
